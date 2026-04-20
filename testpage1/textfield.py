179184e718d9c0fb8d3a0888b3dc658ffeae2a6c
smariharbn-crypto <smariharbn@gmail.com>
2026-04-20 14:50:44 +0530

    my second commit

--- testpage1/textfield.py
+++ testpage1/textfield.py
@@ -3,6 +3,7 @@ import browser_setup
 class Automatetextfield(Baseclass):
     def textfield(self):
         print('hello')
+        print("giving changes from local")
 
 obj = Automatetextfield()
 obj.textfield()
