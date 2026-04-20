5d96871cabf31bcbf5ec1f8c6c0b367f881487a5
smariharbn-crypto <smariharbn@gmail.com>
2026-04-20 14:52:50 +0530

    Update print statement in textfield method

--- testpage1/textfield.py
+++ testpage1/textfield.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import browser_setup
 class Automatetextfield(Baseclass):
     def textfield(self):
-        print('sudhi')
+        print('This is my print statment from remote repo')
 
 obj = Automatetextfield()
 obj.textfield()
